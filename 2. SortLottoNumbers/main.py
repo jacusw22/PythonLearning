1bfef18d262f5a92ab1462d3b1286128db97c625
Grzegorz Pietrzak <kontakt@gpietrzak.pl>
2020-05-28 00:04:12 +0200

    2nd: add: ask user for numbers, cast to int, print array with these numbers

--- 2. SortLottoNumbers/main.py
+++ 2. SortLottoNumbers/main.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 
+print("Podaj 6 liczb z zakresu 1 - 49 oddzielone spacjami a ja je posortuje:")
+
+numbers = input("Moje liczny to: ")
+
+tab_numbers = numbers.split(" ")
+sorted_numbers = []
+for number in tab_numbers:
+    print(int(number))
+    sorted_numbers.append(number)
+
+
+print(sorted_numbers)
+
+
